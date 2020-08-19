7685b6b37cf0df345f61255c71814cbf51b86219
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2020-08-19 16:10:58 +0530

    Create shorthand.java
    short hand is used to assign a datatype of first operand as a result datatype.

---
+++ shorthand.java
@@ -0,0 +1,8 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    byte b=8;
+	    b*=2.5;// b=b+2.5; 2.5 is double but 5 is byte so we use shorthand
+		System.out.println(b);
+	}
+}
