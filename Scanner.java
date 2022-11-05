c68a9b0b284a1ae6ea3adb41fb10201fa65c5764
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2022-11-05 20:09:55 +0530

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class HelloWorld {       //class
+    public static void main(String[] args) { //main method
+    Scanner sc=new Scanner(System.in);
+        int a =sc.nextInt(); // variable
+        int b= sc.nextInt(); 
+        System.out.println(a+b);
+    }
+}
