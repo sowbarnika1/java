ad3a3700e3ab638a213a31ca7f87f2f72ac3e7f5
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2022-11-05 20:07:03 +0530

    Create Class_ex.java

---
+++ Class_ex.java
@@ -0,0 +1,7 @@
+class HelloWorld {       //class
+    public static void main(String[] args) { //main method
+        int a =10; // variable
+        int b= 20; 
+        System.out.println(a+b);
+    }
+}
