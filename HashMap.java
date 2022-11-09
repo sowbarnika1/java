e464757f085dbdde6f803f6bfdc7dd9c09c931e4
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2022-11-09 15:57:37 +0530

    Create HashMap.java

---
+++ HashMap.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+class HashMap{
+	public static void main(String []argh)
+	{
+		Scanner in = new Scanner(System.in);
+		int n=in.nextInt();
+		in.nextLine();
+        HashMap<String,Integer> h=new HashMap<>();
+		for(int i=0;i<n;i++)
+		{
+			String name=in.nextLine();
+			int phone=in.nextInt();
+            h.put(name,phone);
+            in.nextLine();
+        }
+		while(in.hasNext())
+		{
+			String s=in.nextLine();
+            if(h.get(s)!=null)
+            {
+                System.out.println(s+"="+h.get(s));
+            }
+            else
+            {
+                System.out.println("Not found");
+
+            }
+
+		}
+        
+	}
+}
